78733e18c95575514c5fd1cb75ecf509ce39360b
Patricia <Patricia>
2015-12-23 11:40:00 -0700

    Week two updates to the practice website. Things include: custom post type, advanced custom fields, and custom archive/content page.

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying the case studies archive page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<ul class="archive-case-studies">
+			<?php while ( have_posts() ) : the_post();
+				$image_1 = get_field("image_1");
+				$size = "full"; 
+				$services = get_field('services');
+			?>
+				<li class="individual-case-studies">
+				<article class="case-study">
+					<aside class="case-study-archive-sidebar">
+						<h2><a href="<?php the_permalink() ?>"><?php the_title();?></a></h2>
+						<h5><?php echo $services; ?></h5>
+						<?php the_excerpt(); ?>
+						<p> <strong><a href="<?php the_permalink(); ?>">View Project</a></strong></p>
+					</aside>						
+					<div class="case-study-archive-images">
+						<a href="<?php the_permalink(); ?>">
+							<?php if($image_1) {
+								echo wp_get_attachment_image($image_1, $size);
+							 }?>
+						</a>
+					</div>					
+				</article>
+			</li>
+			<?php endwhile; // end of the loop. ?>
+		</ul>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
